cb8cc88266eb57d6921af60ce2b58a58d1d0a491
Sir-vision <111023652+Sir-vision@users.noreply.github.com>
2022-10-19 04:01:40 +0100

    Update main.h

--- main.h
+++ main.h
@@ -23,26 +23,26 @@ typedef struct flags
  * struct printHandler - struct to choose the right function depending
  * on the format specifier passed to _printf()
  * @c: format specifier
- * @f: pointer to the correct printing function
+ * @flg: pointer to the correct printing function
  */
 
 typedef struct printHandler
 {
 	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
+	int (*flg)(va_list ap, flags_t *flg);
+} printHandler;
 
 /* print_nums */
-int print_int(va_list l, flags_t *f);
+int print_int(va_list l, flags_t *flg);
 void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
+int print_unsigned(va_list l, flags_t *flg);
 int count_digit(int i);
 
 /* print_bases */
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
+int print_hex(va_list l, flags_t *flg);
+int print_hex_big(va_list l, flags_t *flg);
+int print_binary(va_list l, flags_t *flg);
+int print_octal(va_list l, flags_t *flg);
 
 /* converter */
 char *convert(unsigned long int num, int base, int lowercase);
@@ -54,25 +54,25 @@ int _printf(const char *format, ...);
 int (*get_print(char s))(va_list, flags_t *);
 
 /* get_flag */
-int get_flag(char s, flags_t *f);
+int get_flag(char s, flags_t *flg);
 
 /* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
+int print_string(va_list l, flags_t *flg);
+int print_char(va_list l, flags_t *flg);
 
 /* write_funcs */
 int _putchar(char c);
 int _puts(char *str);
 
 /* print_custom */
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
+int print_rot13(va_list l, flags_t *flg);
+int print_rev(va_list l, flags_t *flg);
+int print_bigS(va_list l, flags_t *flg);
 
 /* print_address */
-int print_address(va_list l, flags_t *f);
+int print_address(va_list l, flags_t *flg);
 
 /* print_percent */
-int print_percent(va_list l, flags_t *f);
+int print_percent(va_list l, flags_t *flg);
 
 #endif
