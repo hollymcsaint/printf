96abe5dcc26bbbd9d15f76eba65ef078d7ea10c6
Sir-vision <111023652+Sir-vision@users.noreply.github.com>
2022-10-19 04:08:09 +0100

    Update get_print.c

--- get_print.c
+++ get_print.c
@@ -38,6 +38,6 @@ int (*get_print(char s))(va_list, flags_t *)
 
 	for (i = 0; i < flags; i++)
 		if (func_arr[i].c == s)
-			return (func_arr[i].f);
+			return (func_arr[i].flg);
 	return (NULL);
 }
